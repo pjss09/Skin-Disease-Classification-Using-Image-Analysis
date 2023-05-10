397158b15b1cc90ac9cda61eb0feb5a6e049d0bc
pjss09 <74112194+pjss09@users.noreply.github.com>
2023-05-10 16:30:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import requests
+app= FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+MODEL = tf.keras.models.load_model("../saved_models/7")
+CLASS_NAMES = ["Basal cell carcinoma", "Melanocytic nevus", "Melanoma"]
+
+@app.get("/ping")
+async def ping():
+    return "Hello"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/Predict")
+async def Predict(
+    file:UploadFile=File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    predictions = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = float(np.max(predictions[0]))
+
+    return {
+        'class': predicted_class,
+        'confidence': confidence
+    }
+    
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
